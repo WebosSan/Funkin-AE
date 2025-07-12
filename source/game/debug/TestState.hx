c46836d8c693badb916f003344a954c37e3d824b
WebosSan <manueloctavo880@gmail.com>
2025-07-12 12:49:31 -0500

    some modifications

--- source/game/debug/TestState.hx
+++ source/game/debug/TestState.hx
@@ -11,7 +11,18 @@ class TestState extends FlxState{
         //Path shits
 
         #if desktop
+		// How FileSystem looks
         trace(FileSystem.readDirectory(Paths.getPath("data")));
+		// How Paths loooks
+		trace(Paths.readDirectory("data"));
+
+		trace(Paths.getText("here goes data"));
         #end
+		var dad:FlxSprite = new FlxSprite();
+		dad.frames = Paths.getSparrowAtlas("daddy dearest/spritesheet", "data/characters");
+		dad.screenCenter();
+		dad.animation.addByPrefix("idle", "Dad idle dance", 24);
+		dad.animation.play("idle");
+		add(dad);
     }
 }
